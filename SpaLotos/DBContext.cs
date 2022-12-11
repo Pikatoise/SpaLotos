c9d2b9f8828a2c66eb2d15ff526205743f9d1e88
Pikatoise <wierwolf@hotmail.com>
2022-12-11 16:10:00 +0500

    Добавьте файлы проекта.

---
+++ SpaLotos/DBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace SpaLotos
+{
+    public class DBContext
+    {
+        MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
+
+        public bool ConnectionStatus()
+        {
+            try
+            {
+                connection.Open();
+                connection.Close();
+            } catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SpaLotos/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Threading;
+using System.Windows.Media.Animation;
+using System;
+using System.Windows.Media.Effects;
+
+namespace SpaLotos
+{
+    public partial class AuthWindow : Window
+    {
+        DBContext db = new DBContext();
+
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!db.ConnectionStatus())
+            {
+
+            }
+        }
+    }
+}
