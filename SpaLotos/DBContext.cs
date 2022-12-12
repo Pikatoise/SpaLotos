285e411888cf00d891650b1f1c08b97f421cd6d1
Pikatoise <wierwolf@hotmail.com>
2022-12-12 08:16:24 +0500

    ...

--- SpaLotos/DBContext.cs
+++ SpaLotos/DBContext.cs
@@ -5,25 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Configuration;
+using System.Windows;
 
 namespace SpaLotos
 {
     public class DBContext
     {
-        MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
+        MySqlConnection connection;
+        public bool Status 
+        { 
+            get 
+            {
+                try
+                {
+                    connection.Open();
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
 
-        public bool ConnectionStatus()
+                return true;
+            } 
+        }
+
+        public DBContext()
         {
             try
             {
+                connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
+            } catch (Exception e) {}
+        }
+
+        public string SoloRequest(string request)
+        {
+            MySqlCommand command = new MySqlCommand(request, connection);
+            string result = null;
+
+            try
+            {
                 connection.Open();
+
+                object response = command.ExecuteScalarAsync().Result;
+
+                if (!(response == null))
+                    result = response.ToString();
+
                 connection.Close();
+
             } catch (Exception e)
             {
-                return false;
+                MessageBox.Show($"{e.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
-            return true;
+            return result;
+        }
+
+        public MySqlDataReader MultyRequest()
+        {
+            return null;
         }
     }
 }
