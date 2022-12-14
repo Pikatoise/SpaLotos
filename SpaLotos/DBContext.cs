1ca9c1b38e1f775bf349c058db76e66d5fdcd7a9
Pikatoise <wierwolf@hotmail.com>
2022-12-14 10:28:25 +0500

    Добавьте файлы проекта.

---
+++ SpaLotos/DBContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Windows;
+using System.Data;
+
+namespace SpaLotos
+{
+    public class DBContext
+    {
+        MySqlConnection connection;
+        public bool Status 
+        { 
+            get 
+            {
+                try
+                {
+                    connection.OpenAsync();
+                    connection.CloseAsync();
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+
+                return true;
+            } 
+        }
+
+        public DBContext()
+        {
+            try
+            {
+                connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
+            } catch (Exception e) {}
+        }
+
+        public string SoloRequest(string request)
+        {
+            MySqlCommand command = new MySqlCommand(request, connection);
+            string result = null;
+
+            try
+            {
+                connection.OpenAsync();
+
+                object response = command.ExecuteScalarAsync().Result;
+
+                if (response != null)
+                    result = response.ToString();
+
+                connection.CloseAsync();
+
+            } catch (Exception e)
+            {
+                MessageBox.Show($"{e.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return result;
+        }
+
+        public DataTable TableRequest(string request)
+        {
+            MySqlCommand command = new MySqlCommand(request, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable table = new DataTable();
+
+            try
+            {
+                connection.OpenAsync();
+                adapter.Fill(table);
+                connection.CloseAsync();
+
+            } catch(Exception e)
+            {
+                MessageBox.Show($"{e.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return table;
+        }
+
+        public MySqlDataReader MultyRequest(string request)
+        {
+            return null;
+        }
+    }
+}
---
+++ SpaLotos/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpaLotos
+{
+    public partial class MainWindow : Window
+    {
+        Grid currentPage;
+        Button currentMenuItem;
+        TextBlock statusBlock;
+
+        string role;
+        DBContext db;
+
+        public MainWindow(string Role, DBContext db)
+        {
+            InitializeComponent();
+            this.Height = SystemParameters.PrimaryScreenHeight*0.95;
+            this.Width = SystemParameters.PrimaryScreenWidth;
+
+            currentPage = ClientsPage;
+            currentMenuItem = ClientsButton;
+            statusBlock = StatusBlock;
+
+            role = Role;
+            this.db = db;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StatusUpdate();
+
+            if (role.Equals("admin"))
+                UserBlock.Text = "Пользователь: Администратор";
+            else
+            {
+                UserBlock.Text = "Пользователь: Оператор-кассир";
+                AdminButton.Visibility = Visibility.Collapsed;
+            }
+
+            GridFill(ClientsGrid, "SELECT * FROM Clients");
+        }
+
+        private void AdminButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (db.Status)
+            {
+                ChangePage(AdminPage,AdminButton);
+            }
+            else
+            {
+                StatusUpdate();
+            }
+        }
+
+        private void ClientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (db.Status)
+            {
+                ChangePage(ClientsPage, ClientsButton);
+
+            }
+            else
+            {
+                StatusUpdate();
+            }
+
+            GridFill(ClientsGrid, "SELECT * FROM Clients");
+        }
+
+        private void ServicesButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (db.Status)
+            {
+                ChangePage(ServicesPage, ServicesButton);
+            }
+            else
+            {
+                StatusUpdate();
+            }
+        }
+
+        private void ServenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (db.Status)
+            {
+                ChangePage(ServePage, ServeButton);
+            }
+            else
+            {
+                StatusUpdate();
+            }
+        }
+
+        private void AddClientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(FioClientsBox.Text) && !string.IsNullOrWhiteSpace(ContactClientsBox.Text) && !string.IsNullOrWhiteSpace(AgeClientsBox.Text) && !string.IsNullOrWhiteSpace(GenderClientsBox.Text) && GenderClientsBox.Text.Length == 1)
+            {
+                bool isExist = false;
+                foreach (object o in ClientsGrid.Items)
+                    if ((o as DataRowView).Row.ItemArray[2].ToString().Equals(ContactClientsBox.Text))
+                    {
+                        MessageBox.Show("Клиент с таким контактом уже существует!");
+                        isExist = true;
+                        break;
+                    }
+                if (!isExist)
+                {
+                    db.SoloRequest($"INSERT INTO Clients(FullName,Contact,Age,Gender) VALUES ('{FioClientsBox.Text}','{ContactClientsBox.Text}',{AgeClientsBox.Text},'{GenderClientsBox.Text}')");
+                    GridFill(ClientsGrid, "SELECT * FROM Clients");
+                }
+            }
+            else
+                MessageBox.Show("Неверные данные!");
+        }
+
+        void ChangePage(Grid nextPage, Button nextMenuButton)
+        {
+            currentMenuItem.IsEnabled= true;
+            currentMenuItem = nextMenuButton;
+            currentMenuItem.IsEnabled= false;
+
+            currentPage.Visibility= Visibility.Hidden;
+            currentPage = nextPage;
+            currentPage.Visibility= Visibility.Visible;
+        }
+
+        void StatusUpdate()
+        {
+            if (db.Status)
+            {
+                statusBlock.Text = "Подключен";
+                statusBlock.Foreground = Brushes.LimeGreen;
+            }
+            else
+            {
+                statusBlock.Text = "Отключен";
+                statusBlock.Foreground = Brushes.Red;
+            }
+        }
+
+        void GridFill(DataGrid grid, string request)
+        {
+            grid.ItemsSource = null;
+            grid.ItemsSource = db.TableRequest(request).DefaultView;
+        }
+
+    }
+}
